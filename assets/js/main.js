01ad849ba8c49c3828c341b2d6a57a8418fad434
Marc Ribas <ribascode@gmail.com>
2021-02-27 13:52:34 +0100

    add button funcionalitat

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,28 @@
 'use strict'
 
+const addBtn = document.getElementById("add-btn")
+const templateToDoList = document.getElementById('template-to-do-list').content
+
+const cards = document.getElementById('cards')
+const fragment = document.createDocumentFragment()
+
+
+
+addBtn.addEventListener('click', () => {
+    pintarToDoList();
+});
+
+
+
+const pintarToDoList = () => {
+
+    const clone = templateToDoList.cloneNode(true)
+    
+    fragment.appendChild(clone)
+    cards.appendChild(fragment)
+
+}
+
+
+
+let list = ["task 1"]
